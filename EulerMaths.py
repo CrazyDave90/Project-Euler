781a21a66a20a4a3b002f4b1f721f848142de833
David Whiting <dwhiting06@gmail.com>
2015-02-07 03:41:15 -0500

    Fixed fibCache Numbers
    1) fibCache dictionary numbers were incorrectly shifted by one.

--- EulerMaths.py
+++ EulerMaths.py
@@ -1,6 +1,6 @@
 import gmpy2
 import math
-fibCache = {1:0, 2:1, 3:1, 4:2, 5:3, 6:5, 7:8, 8:13, 9:21, 10:34}
+fibCache = {0:0, 1:1, 2:1, 3:2, 4:3, 5:5, 6:8, 7:13, 8:21, 9:34}
 
 
 def listMultiples(multiple=1, lowerBound=0, upperBound=math.pow(10, 6)):
