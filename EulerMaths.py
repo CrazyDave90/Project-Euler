5ee97f47cf929a3833f901c4ce500be7847e3465
David Whiting <dwhiting06@gmail.com>
2015-02-07 03:15:16 -0500

    Added Functions
    1) Added nthPrime() function which returns the nth prime.
    2) Added the greatestCommonFactor() and leastCommonMultiple() functions
    using the gmpy2 library.
    3) Changed the isPrime() function to use the gmpy2 library.
    4) Changed the listFactors() function to not return 1
    5) Misc changes in style/efficiency.

--- EulerMaths.py
+++ EulerMaths.py
@@ -1,8 +1,9 @@
+import gmpy2
 import math
 fibCache = {1:0, 2:1, 3:1, 4:2, 5:3, 6:5, 7:8, 8:13, 9:21, 10:34}
 
 
-def listMultiples(multiple=1, lowerBound=0, upperBound=10**6):
+def listMultiples(multiple=1, lowerBound=0, upperBound=math.pow(10, 6)):
     return [n for n in range(lowerBound, upperBound, multiple)]
 
 
@@ -13,19 +14,35 @@ def fibonacci(number):
 
 
 def listFactors(number):
-    factors = [1, number]
-    for n in range(2, int(math.sqrt(number)) + 1):
+    factors = [number]
+    for n in range(2, int(math.sqrt(number))+1):
         if (number % n == 0):
             factors.append(n)
-            factors.append(number/n)
+            factors.append(int(number/n))
     return factors
 
 
 def isPrime(number):
-    if (len(listFactors(number)) > 2):
-        return False
-    return True
+    return gmpy2.is_prime(number)
 
 
 def listPrimeFactors(number):
     return [n for n in listFactors(number) if isPrime(n)]
+
+
+def nthPrime(n):
+    numberOfPrime = 1
+    result = 2
+    while n > numberOfPrime:
+        result = gmpy2.next_prime(result)
+        numberOfPrime += 1
+    return int(result)
+
+
+def greatestCommonFactor(a, b):
+    return int(gmpy2.gcd(a, b))
+
+
+def leastCommonMultiple(a, b):
+    return int(gmpy2.lcm(a, b))
+
