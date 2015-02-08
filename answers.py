9bb3e050071b4ecf4d52fbe8fa951e39a475f523
David Whiting <dwhiting06@gmail.com>
2015-02-08 13:53:06 -0500

    Added four new problems
    1) Added Problems 4, 6, 8 and 9
    2) added p8.txt which is a text file containing the 1000 digit number
    used in problem 8

--- answers.py
+++ answers.py
@@ -1,6 +1,8 @@
 import eulermaths
 import functools
 import gmpy2
+import math
+import operator
 
 
 def problem1():
@@ -23,12 +25,53 @@ def problem3():
     return max(eulermaths.listPrimeFactors(600851475143))
 
 
+def problem4():
+    answer = 0
+    for i in range(100, 1000):
+        for j in range(i, 1000):
+            number = i*j
+            if (str(number) == str(number)[::-1] and number > answer):
+                answer = number
+    return answer
+
+
 def problem5():
     return int(functools.reduce(gmpy2.lcm, range(2, 21)))
 
 
+def problem6():
+    sumSquares = 0
+    squareSum = 0
+    for i in range(1, 101):
+        sumSquares += math.pow(i, 2)
+        squareSum += i
+    squareSum = math.pow(squareSum, 2)
+    return squareSum - sumSquares
+
+
 def problem7():
     return eulermaths.nthPrime(10001)
+
+
+def problem8():
+    answer = 0
+    digits = []
+    with open('p8.txt', 'r') as number: # the file containing the 1000 digit number
+        digits.extend(number.read())
+        digits = list(map(int, digits)) # easiest to iterate over list of digits
+    for i in range(0, 988):
+        digitProduct = functools.reduce(operator.mul, digits[i:i+13])
+        if (digitProduct > answer):
+            answer = digitProduct
+    return answer
+
+
+def problem9():
+    for a in range (1, 334):
+        for b in range(i, 666):
+            if (math.pow(a, 2) + math.pow(b, 2) == math.pow(1000-(a+b), 2)):
+                # constraint: since a+b+c = 1000, c = 1000-(a+b)
+                return a*b*(1000-(a+b))
 
 
 def problem10():
@@ -38,4 +81,3 @@ def problem10():
         answer += prime
         prime = int(gmpy2.next_prime(prime))
     return answer
-        
